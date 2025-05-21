797557342b35831f2a655754ac86ed0f54a67615
Mark28676341736 <mkovtun585@gmail.com>
2025-05-21 15:56:10 +0300

    Add files via upload

--- hw3.js
+++ hw3.js
@@ -7,7 +7,7 @@ console.log("Email contains '@':")
 let word1 = "my";
 let word2 = "name";
 let word3 = "is"
-let fullname = `${word1}${word2}${word3} viktor`;
+let fullname = `${word1}${word2}${word3} Mark`;
 console.log("full name", fullName)
 let userName = "Марк";
 let payment = 300
