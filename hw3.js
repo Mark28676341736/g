37a2d721fc3443ab04ba263583a7378b8892b597
Mark28676341736 <mkovtun585@gmail.com>
2025-05-21 15:54:39 +0300

    Add files via upload

---
+++ hw3.js
@@ -0,0 +1,14 @@
+let result  = 5 + 5 + '5'
+console.log("резулбтат")
+console.log("result type")
+let email = "mkovtun585@gmail.com";
+let containsAt = email.includs('@');
+console.log("Email contains '@':")
+let word1 = "my";
+let word2 = "name";
+let word3 = "is"
+let fullname = `${word1}${word2}${word3} viktor`;
+console.log("full name", fullName)
+let userName = "Марк";
+let payment = 300
+alert(`Дякуэмо, {username} До сплати ${payment} грн`)
